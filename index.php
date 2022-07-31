d640a95087bbd0335ab15995b179dab436fd5951
stanley097 <100255309+stanley097@users.noreply.github.com>
2022-07-31 21:07:17 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 
 
-echo "hello stanley, finally you completed the cicd...hurray!!! oops, but yet a lot of work is pending"
+echo "hello stanley, finally you completed the cicd...hurray!!!"
 
 
 ?>
